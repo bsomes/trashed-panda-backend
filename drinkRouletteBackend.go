be86474fe3cd12e072251b93310f0e2febd93969
Brian Somes <bsomes@umich.edu>
2016-12-18 18:05:30 -0500

    added handler for making drink by id list

--- drinkRouletteBackend.go
+++ drinkRouletteBackend.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -51,12 +53,30 @@ func test(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	json.NewEncoder(w).Encode(getTestDrink())
 }
 
+func makeDrinkFromList(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	idString := ps.ByName("ingredients")
+	ids := strings.Split(idString, "-")
+	ingredients := make([]Ingredient, len(ids))
+	allIngredients := getIngredients()
+	for ind, v := range ids {
+		if i, err := strconv.Atoi(v); err == nil {
+			ingredients[ind] = allIngredients[i]
+		}
+	}
+	json.NewEncoder(w).Encode(Drink{
+		Name:     "Test",
+		Contents: uniform(ingredients),
+		Size:     Buttchug,
+	})
+}
+
 func main() {
 	port := os.Getenv("PORT")
 
 	router := httprouter.New()
 	router.GET("/ingredients", getEncodedIngredients)
 	router.GET("/test", test)
+	router.GET("/makedrink/:ingredients", makeDrinkFromList)
 
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
@@ -64,22 +84,22 @@ func main() {
 func getIngredients() []Ingredient {
 	return []Ingredient{
 		Ingredient{
-			ID:   1,
+			ID:   0,
 			Name: "Vodka",
 			Cat:  Alcohol,
 		},
 		Ingredient{
-			ID:   2,
+			ID:   1,
 			Name: "Milk",
 			Cat:  Mixer,
 		},
 		Ingredient{
-			ID:   3,
+			ID:   2,
 			Name: "Kahlua",
 			Cat:  Alcohol,
 		},
 		Ingredient{
-			ID:   4,
+			ID:   3,
 			Name: "Cocoa powder",
 			Cat:  Other,
 		},
