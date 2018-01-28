0e2db5d76e91094ef97916de47f69cd8b4c9fea2
Brian Somes <bsomes@umich.edu>
2018-01-28 17:54:42 -0500

    added shell command to install tensorflow binaries

---
+++ .heroku/run.sh
@@ -0,0 +1,9 @@
+mkdir 'lib'
+TF_TYPE="cpu" # Change to "gpu" for GPU support
+ TARGET_DIRECTORY='./lib'
+ curl -L \
+   "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-linux-x86_64-1.5.0.tar.gz" |
+tar -C $TARGET_DIRECTORY -xz
+
+export LIBRARY_PATH=$LIBRARY_PATH:./lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
