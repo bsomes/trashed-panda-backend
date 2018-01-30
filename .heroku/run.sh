4437195379a6470e5457524ec1e5c066ab48f166
Brian Somes <bsomes@umich.edu>
2018-01-29 21:58:19 -0500

    use ls for stuff

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,7 +1,10 @@
-mkdir /app/lib
-TF_TYPE="cpu" # Change to "gpu" for GPU support
- TARGET_DIRECTORY='/app/lib'
- curl -L \
-   "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-linux-x86_64-1.5.0.tar.gz" |
-tar -C $TARGET_DIRECTORY -xz
+#mkdir /app/lib
+#TF_TYPE="cpu" # Change to "gpu" for GPU support
+# TARGET_DIRECTORY='/app/lib'
+# curl -L \
+#   "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_TYPE}-linux-x86_64-1.5.0.tar.gz" |
+#tar -C $TARGET_DIRECTORY -xz
 
+ls /app/lib
+ls /app/include
+ls /app/libtensorflow
